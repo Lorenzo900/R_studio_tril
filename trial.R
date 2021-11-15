b0aea429cc681493be4899727efb03232529fc6c
Lorenzo <53413642+Lorenzo900@users.noreply.github.com>
2021-11-15 20:30:19 +0100

    uu

--- trial.R
+++ trial.R
@@ -3,4 +3,4 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 
-# per fare tilde su mac premi alt + 5, per fare parentesi graffa premi shift + option + parentesi quadra
+# per fare tilde su mac premi alt + 5, per fare parentesi graffa premi shift + option + parentesi quadra, chiaro?
